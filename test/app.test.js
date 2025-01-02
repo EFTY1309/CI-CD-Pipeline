f9c71cd488d20321a525cf80c3aebb71fcbc9449
efty <efty3222@gmail.com>
2025-01-02 23:33:37 +0600

    practice change

--- test/app.test.js
+++ test/app.test.js
@@ -8,3 +8,10 @@ describe("GET /", () => {
     expect(response.text).toBe("Hello, World this is a practice for CICD!");
   });
 });
+
+describe("GET /non-existent-route", () => {
+  it("should return 404 for non-existent route", async () => {
+    const response = await request(app).get("/non-existent-route");
+    expect(response.statusCode).toBe(404);
+  });
+});
